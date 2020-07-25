2128fdd808bcff23dfb22d74480e5599bb07bef8
infinityboy <451530398@qq.com>
2020-07-25 09:46:09 +0800

    :first: create project

---
+++ README.md
@@ -0,0 +1,19 @@
+# mb-pro
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/About.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="main">
+      移动端适配
+    </div>
+    <van-button type="default">默认按钮</van-button>
+  </div>
+</template>
+<style lang="postcss" scoped>
+.main {
+  width: 200px;
+  height: 400px;
+  background-color: #fff;
+  font-size: 24px;
+}
+.ignore {
+  margin: 10px;
+  background-color: red;
+}
+.hairlines {
+  border-bottom: 0.5px solid blue;
+}
+@svg 1px-border {
+  height: 2px;
+  @rect {
+    fill: var(--color, black);
+    width: 100%;
+    height: 50%;
+  }
+}
+.main {
+  border: 1px solid transparent;
+  border-image: svg(1px-border param(--color #00b1ff)) 2 2 stretch;
+}
+[w-200-400] {
+  width: 200px;
+}
+</style>
